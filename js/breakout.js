e02cfbba70e2eed7a9a215bb3d9690f4c90c6f9b
xionXinYunGitHub <125775287+xionXinYunGitHub@users.noreply.github.com>
2024-05-04 06:18:00 +0800

    Add files via upload

--- js/breakout.js
+++ js/breakout.js
@@ -1,4 +1,4 @@
-alert("这是一个复制粘贴过来的烂游戏！");
+
 const config= {
     type:Phaser.AUTO,
     width:600,
@@ -20,13 +20,17 @@ const config= {
     }
     
 };
-
-
 var game=new Phaser.Game(config);
 var platforms;
 var player;
 var cursors;
+var csm;
 var keyboard = document.getElementById(`keyboard`);
+            function chuansong(player,csm){
+                
+                
+                alert("技术原因，传送失败！");
+            }
 
             function preload(){
                 
@@ -39,24 +43,18 @@ var keyboard = document.getElementById(`keyboard`);
                 //game.physics.startSystem(Phaser.Physics.ARCADE);
                 this.add.image(300,200,`sky`);
                 platforms = this.physics.add.staticGroup();
-                //platforms.enablebody = true;
-                platforms.create(45,315,`csm`);
+                platforms.create(45,310,`csm`);
                 platforms.create(0,380,'ground').refreshBody();
-                //ground.scale.setTo(2,2);
-                //ground.body.immovable=true;
                 platforms.create(0,250,'ground');
-               // ledge.body.immovable=true;
                 platforms.create(500,310,'ground');
-                //ledge.body.immovable=true;
+               
                 //加载主角
                 player=this.physics.add.sprite(0,205,'dude');
-             /* game.physics.arcade.enable(player);
-                player.body.bounce.y=0.2;    //设置主角的弹性
-                player.body.gravity.y=30;    //设置主角的重力
-                player.body.collideWorldBounds=true; */
                 player.setBounce(0.2)
                 player.setCollideWorldBounds(true);
                 this.physics.add.collider(player,platforms);
+               
+                this.physics.add.overlap(player,platforms,chuansong,null,this);
               /*player.animations.add('left',[0,1,2,3],10,true);    //设置主角向左方向的序列帧
                 player.animations.add('right',[5,6,7,8],10,true);*/
                 this.anims.create({
@@ -81,6 +79,7 @@ var keyboard = document.getElementById(`keyboard`);
                 });
                 
                 cursors = this.input.keyboard.createCursorKeys();
+                //this.input.touch.onTouchStart=function(){alert();};
 
             }
             function update(){
@@ -107,7 +106,7 @@ var keyboard = document.getElementById(`keyboard`);
       }
       
       if(cursors.up.isDown && player.body.touching.down){
-        player.setVelocityY(-300);
+        player.setVelocityY(-70);
       }
     
             
@@ -118,22 +117,26 @@ var keyboard = document.getElementById(`keyboard`);
        if(event.target.tagName == `BUTTON`){
            var value = event.target.textContent;
            if(value == `上`){
-               player.setVelocityY(-50);
-               
+               //player.setVelocityY(-60);
+               cursors.up.isDown=true;
+               player.body.touching.down = true;
            }else if(value == `下`){
-               player.setVelocityY(50);
+               //player.setVelocityY(50);
                
            }else if(value == `左`){
-               player.setVelocityX(-1000);
-               player.anims.play("left",true);
-               
+               /*player.setVelocityX(-50);
+               player.anims.play("left",true);*/
+               cursors.left.isDown=true;
            }else if(value == `右`){
-               player.setVelocityX(1000);
-               player.anims.play("right",true);
-               
+              /* player.setVelocityX(50);
+               player.anims.play("right",true);*/
+               cursors.right.isDown = true;
            }else{
-               
-               player.anims.play('turn');
+              /* player.setVelocityX(0);
+               player.anims.play('turn');*/
+               cursors.up.isDown=false;
+               cursors.left.isDown=false;
+               ursors.right.isDown = false;
            }
            
            
